e211f72583272b9c59f7beb1b3e2d497cfb9d909
Hohichh <verkovich2005@gmail.com>
2025-04-12 02:41:34 +0300

    corpus document class added

--- lab2_bot/services/morph_analizer/CorpusDoc.py
+++ lab2_bot/services/morph_analizer/CorpusDoc.py
@@ -1,6 +1,7 @@
 from docx import Document
 
 from morph_analizer import Dictionary
+from morph_analizer import nlp
 
 class CorpusDoc:
     def __init__(self, doc: Document):
@@ -8,7 +9,7 @@ class CorpusDoc:
         self.author: str
         self.topic: str = "Gastronomy"
         self.text: str = self.__load_text(doc)
-        self.__dictionary = Dictionary(self.text)
+        self.__marking = Dictionary(self.text)
     
     def __load_text(self, doc: Document) -> str:
         document = doc
@@ -23,15 +24,30 @@ class CorpusDoc:
 
     def __get_lemma_stats(self, word:str) -> int:
         #search all matches with word's lemma. 
-        pass
+        nltk_word = nlp(word)[0]
+        counter: int = 0
+
+        for lexeme in self.__marking.dictionary.values():
+            if lexeme.lemma == nltk_word.lemma_:
+                counter += lexeme.count
+
+        return counter
 
     def __get_word_form_stats(self, word: str) -> int:
         #search all matches with current word form
-        pass
+        nltk_word = nlp(word)[0]
+        counter: int = 0
+
+        for word_form in self.__marking.dictionary.keys():
+            if nltk_word.text == word_form:
+                counter = self.__marking.dictionary[word_form].count
+                return counter
 
     def __get_concordance_list(self, word:str) -> list[str]:
-        pass
+        #TODO : придумать как извлечь конкордансный список
+        return "..." + word + "..."
 
     def pretty_print_stats(self, word: str):
-        #
+        lemma_count = self.__get_lemma_stats(word)
+        word_form_count = self.__get_word_form_stats(word)
         pass

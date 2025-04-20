cc0efcb986f88ae45df2ecd4fc06ce8b6c5d9c63
Hohichh <verkovich2005@gmail.com>
2025-04-20 22:24:41 +0300

    add async file loading

--- lab2_bot/services/morph_analizer/CorpusDoc.py
+++ lab2_bot/services/morph_analizer/CorpusDoc.py
@@ -1,100 +1,106 @@
 from docx import Document
+import aiofiles
 
 from .Dictionary import Dictionary, nlp
 from .Lexeme import Lexeme
 from lexicon import LEXICON_RU
 
 class CorpusDoc:
-    def __init__(self, doc: Document):
+    def __init__(self, path: str):
         self.title: str = ""
         self.author: str = ""
         self.topic: str = "Gastronomy"
-        self.text: str = self.__load_text(doc)
-        self.__marking = Dictionary(self.text)
+        self._text: str = None
+        self._marking: Dictionary = None
+        self._path = path
 
-    def pretty_print_stats(self, word: str) -> str:
-        lemma_count:str = str(self.get_lemma_stats(word))
-        word_form_count:str = str(self.get_word_form_stats(word))
-        concordance_list:str = "\n".join(self.get_concordance_list(word))
-        
-        morph_info = self.get_morph_info(word)
+    @property
+    async def text(self) -> str:
+        if self._text is None:
+            async with aiofiles.open(self._path, encoding="utf-8") as file:
+                self._text = await file.read()
+        return self._text
 
-        return LEXICON_RU["doc_stats"].format(word_form=word, 
-                                              morph_str=morph_info,
-                                              lemmas=lemma_count,
-                                              forms=word_form_count,
-                                              concordances=concordance_list)
-    
-    def get_morph_info(self, word:str) -> str:
+    @property
+    async def marking(self) -> Dictionary:
+        if self._marking is None:
+            self._marking = Dictionary(await self.text)
+        return self._marking
+
+    async def pretty_print_stats(self, word: str) -> str:
+        lemma_count: str = str(await self.get_lemma_stats(word))
+        word_form_count: str = str(await self.get_word_form_stats(word))
+        concordance_list: str = "\n".join(await self.get_concordance_list(word))
+        morph_info = await self.get_morph_info(word)
+
+        return LEXICON_RU["doc_stats"].format(
+            word_form=word,
+            morph_str=morph_info,
+            lemmas=lemma_count,
+            forms=word_form_count,
+            concordances=concordance_list
+        )
+
+    async def get_morph_info(self, word: str) -> str:
+        marking = await self.marking  # Use the property here
         try:
-            lexeme: Lexeme = self.__marking.dictionary[word]
+            lexeme: Lexeme = marking.dictionary[word]
             return lexeme.pretty_print()
         except KeyError:
             nltk_word = nlp(word)[0]
             lexeme = Lexeme(nltk_word.lemma_, nltk_word.pos_)
             return lexeme.pretty_print()
 
-    
-    def __load_text(self, doc: Document) -> str:
-        document: Document = doc
-
-        full_text = []
-        for paragraph in document.paragraphs:
-            full_text.append(paragraph.text)
-
-        text = "\n".join(full_text)
-        return text
-
-    def get_lemma_stats(self, word:str) -> int:
-        #search all matches with word's lemma. 
+    async def get_lemma_stats(self, word: str) -> int:
         nltk_word = nlp(word)[0]
+        marking = await self.marking  # Use the property here
         counter: int = 0
 
-        for lexeme in self.__marking.dictionary.values():
+        for lexeme in marking.dictionary.values():
             if lexeme.lemma == nltk_word.lemma_:
                 counter += lexeme.count
 
         return counter
 
-    def get_word_form_stats(self, word: str) -> int:
-        #search all matches with current word form
+    async def get_word_form_stats(self, word: str) -> int:
         nltk_word = nlp(word)[0]
+        marking = await self.marking  # Use the property here
         counter: int = 0
 
-        for word_form in self.__marking.dictionary.keys():
+        for word_form in marking.dictionary.keys():
             if nltk_word.text == word_form:
-                counter = self.__marking.dictionary[word_form].count
+                counter = marking.dictionary[word_form].count
                 break
-        
+
         return counter
 
-    def get_concordance_list(self, sub_str:str) -> list[str]:
+    async def get_concordance_list(self, sub_str: str) -> list[str]:
         concordance_list = []
-        tokens = self.text.split() #слово / слово+запятая/точка / дефис
-        sub_tokens = sub_str.split() #слово / слово+запятая/точка / дефис
-        for i in range(len(tokens)-len(sub_tokens)+1):
-            tokens_cut_str = " ".join(tokens[i:i+len(sub_tokens)]).lower().replace(".","")
+        text = await self.text  # Use the property here
+        tokens = text.split()
+        sub_tokens = sub_str.split()
+
+        for i in range(len(tokens) - len(sub_tokens) + 1):
+            tokens_cut_str = " ".join(tokens[i:i+len(sub_tokens)]).lower().replace(".", "")
             if sub_str.lower() in tokens_cut_str:
-                concordance: str = self.__extract_context(tokens, i, i+len(sub_tokens)-1,5)
+                concordance = self.__extract_context(tokens, i, i+len(sub_tokens)-1, 5)
                 concordance_list.append(concordance)
 
         return concordance_list
-    
 
-    def __extract_context(self, tokens: list[str], st_ind:int, end_ind, context: int) -> str:
-        #for start/end of the text
-        left_context = tokens[:st_ind]  
-        if len(left_context) > context:  
-            left_context = left_context[st_ind-context:st_ind]
+    def __extract_context(self, tokens: list[str], st_ind: int, end_ind: int, context: int) -> str:
+        left_context = tokens[:st_ind]
+        if len(left_context) > context:
+            left_context = left_context[st_ind - context:st_ind]
+
         if len(tokens[end_ind:]) < context:
             right_context = tokens[end_ind+1:]
         else:
             right_context = tokens[end_ind+1:end_ind+context+1]
-        
-        #for start/end of the sentence
-        str_left_context: str = " ".join(left_context)
-        str_right_context: str = " ".join(right_context)
-        curr_substr: str = " ".join(tokens[st_ind:end_ind+1])
+
+        str_left_context = " ".join(left_context)
+        str_right_context = " ".join(right_context)
+        curr_substr = " ".join(tokens[st_ind:end_ind+1])
 
         return "..." + str_left_context + " " + curr_substr + " " + str_right_context + "..."
         
--- lab2_bot/services/morph_analizer/CorpusManager.py
+++ lab2_bot/services/morph_analizer/CorpusManager.py
@@ -1,24 +1,58 @@
 from docx import Document
+from pathlib import Path
+import uuid
+import aiofiles
 
 from .CorpusDoc import CorpusDoc
 from lexicon import LEXICON_RU
 
 class CorpusManager:
-    def __init__(self): 
-        self.document_list : list[CorpusDoc] = []
+    def __init__(self, user_id: str): 
+        self.user_id = user_id
+        self.user_dir: str = Path(__file__).resolve().parents[2] / "corpus_data" / self.user_id
+        self.user_dir.mkdir(parents=True, exist_ok=True)
+        self.document_list: list[CorpusDoc] = []
 
-    def add_doc(self, doc: CorpusDoc) -> bool:
+    async def add_doc(self, doc: Document, title: str, author: str) -> bool:
         try:
-            self.document_list.append(doc)
+            # взяли текст из ворд файла
+            full_text = []
+            for paragraph in doc.paragraphs:
+                full_text.append(paragraph.text)
+
+            text = "\n".join(full_text)
+
+            path_to_text = self.user_dir / f"{uuid.uuid4()}.txt"
+            async with aiofiles.open(path_to_text, "w", encoding="utf-8") as file:
+                await file.write(text)  # записали
+
+            corpusDoc = CorpusDoc(path_to_text)  # связываем инстанс класса с файлом
+            corpusDoc.title = title
+            corpusDoc.author = author
+
+            self.document_list.append(corpusDoc)  # добавили в список доков
             return True
-        except Exception:
+        except IOError as e:
+            print(f"IO error: {e}")
+        except Exception as e:
+            print(f"something wrong: {e}")
+        finally:
             return False
 
     def delete_doc(self, ind: int) -> bool:
         try:
-            self.document_list.pop(ind)
+            temp_doc = self.document_list.pop(ind)
+            file_path = Path(temp_doc._path)
+            file_path.unlink()  # синхронное удаление файла
+
             return True
-        except IndexError:
+        except FileNotFoundError:
+            print(f"Файл {file_path} не найден.")
+        except PermissionError:
+            print(f"Нет прав для удаления файла {file_path}.")
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+        finally:
             return False
 
     def get_doc(self, ind: int) -> CorpusDoc | None:
@@ -32,37 +66,39 @@ class CorpusManager:
         name_list = []
         for doc in self.document_list:
             name_list.append(doc.title)
-        
+
         return name_list
 
-    def pretty_print_stats(self, word:str) -> str:
-        lemma_count:str = str(self.__get_lemma_stats(word))
-        forms_count:str = str(self.__get_word_form_stats(word))
-        concordance_list:str = "\n".join(self.__get_concordance_list(word))
-        morph_info = self.document_list[0].get_morph_info(word)
-        return LEXICON_RU["corpus_stats"].format(word_form=word,
-                                                 morph_str=morph_info,
-                                                 lemmas=lemma_count,
-                                                 forms=forms_count,
-                                                 concordances=concordance_list)
+    async def pretty_print_stats(self, word: str) -> str:
+        lemma_count: str = str(await self.__get_lemma_stats(word))  # добавлен await
+        forms_count: str = str(await self.__get_word_form_stats(word))  # добавлен await
+        concordance_list: str = "\n".join(await self.__get_concordance_list(word))  # добавлен await
+        morph_info = await self.document_list[0].get_morph_info(word)  # добавлен await
+        return LEXICON_RU["corpus_stats"].format(
+            word_form=word,
+            morph_str=morph_info,
+            lemmas=lemma_count,
+            forms=forms_count,
+            concordances=concordance_list
+        )
 
-    def __get_lemma_stats(self, word:str) -> int:
+    async def __get_lemma_stats(self, word: str) -> int:
         lemmas = 0
         for doc in self.document_list:
-            lemmas += doc.get_lemma_stats(word)
+            lemmas += await doc.get_lemma_stats(word)  # добавлен await
 
         return lemmas
 
-    def __get_word_form_stats(self, word:str) -> int:
+    async def __get_word_form_stats(self, word: str) -> int:
         word_forms = 0
         for doc in self.document_list:
-            word_forms += doc.get_word_form_stats(word)
+            word_forms += await doc.get_word_form_stats(word)  # добавлен await
 
         return word_forms
 
-    def __get_concordance_list(self, sub_str:str) -> list[str]:
+    async def __get_concordance_list(self, sub_str: str) -> list[str]:
         concordance_list = []
         for doc in self.document_list:
-            concordance_list.extend(doc.get_concordance_list(sub_str))
+            concordance_list.extend(await doc.get_concordance_list(sub_str))  # добавлен await
 
         return concordance_list
